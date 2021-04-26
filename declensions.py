37143f491d18721087907186f2b1bd1547a82e2a
Davit <DatoJanez@users.noreply.github.com>
2021-04-26 23:43:21 +0400

    Update declensions.py

--- declensions.py
+++ declensions.py
@@ -59,9 +59,22 @@ def get_declensions_ka(word):
 # For the words containing ['o', 'u' ]      ['nu',   'nun', 'ya', 'da', 'dan']
 # ending with vowels
 
+# ending with consonants
+
+# For the words containing ['ə', 'e', 'i']  ['i',    'in',  'ə',  'də', 'dən']   as the last vowel in the word 
+# For the words containing ['ö', 'ü' ]      ['ü',    'ün',  'ə',  'də', 'dən']
+# For the words containing ['a', 'ı' ]      ['ı',    'ın',  'a',  'da', 'dan']
+# For the words containing ['o', 'u' ]      ['u',    'un',  'a',  'da', 'dan']
+# For the words containing ['ə', 'e', 'i']  ['ni',   'nin', 'yə', 'də', 'dən']
+# For the words containing ['ö', 'ü' ]      ['nü',   'nün', 'yə', 'də', 'dən']
+# For the words containing ['a', 'ı' ]      ['nı',   'nın', 'ya', 'da', 'dan']
+# For the words containing ['o', 'u' ]      ['nu',   'nun', 'ya', 'da', 'dan']
+# ending with vowels
+
 def get_declensions_az(word):
+    is_phrase = ' ' in word
     declensions = []
-    vowels = ['i','ü','e','ö','ə','a','o','u','ı','i:','e:','ö:','ə:','a:','u:']
+    vowels = ['i','ü','e','ö','ə','a','o','u','ı']
     last_char = word[-1:]
     
     last_vowel = ''
@@ -77,13 +90,16 @@ def get_declensions_az(word):
         return declensions
 
     if last_char == 'q':    
+        # does not this rule works here?
         root_ = word[:-1] + 'ğ'
     elif last_char == 'k':
         root_ = word[:-1] + 'y'
     else:
         root_ = word
-
-    if last_char in vowels:
+# nin ne nde nedn
+    # print("is_phrase", is_phrase, ~is_phrase)
+    # print("last_vowel", last_vowel, last_char in vowels)
+    if last_char in vowels and not is_phrase:
         if last_vowel in ['ə', 'e', 'i']:
             declensions = ['ni', 'nin', 'yə', 'də', 'dən']
         if last_vowel in ['ö', 'ü' ]:
@@ -91,7 +107,18 @@ def get_declensions_az(word):
         if last_vowel in ['a', 'ı' ]:
             declensions = ['nı', 'nın', 'ya', 'da', 'dan']
         if last_vowel in ['o', 'u' ]:
+            # Sülhquruculuğu
+            #where this additional N-s come from?
             declensions = ['nu', 'nun', 'ya', 'da', 'dan']
+    elif last_char in vowels and is_phrase:
+        if last_vowel in ['ə', 'e', 'i']:
+            declensions = ['ni', 'nin', 'nə', 'ndə', 'ndən']
+        if last_vowel in ['ö', 'ü' ]:
+            declensions = ['nü', 'nün', 'nə', 'ndə', 'ndən']
+        if last_vowel in ['a', 'ı' ]:
+            declensions = ['nı', 'nın', 'na', 'nda', 'ndan']
+        if last_vowel in ['o', 'u' ]:
+            declensions = ['nu', 'nun', 'na', 'nda', 'ndan']
     else:
         if last_vowel in ['ə', 'e', 'i']:
             declensions = ['i', 'in', 'ə', 'də', 'dən']
